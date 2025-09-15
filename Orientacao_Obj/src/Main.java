66f26972ed21c21f960be81d81d56e868d5b8269
renan <rm93744@fiap.com.br>
2025-09-15 01:00:01 -0300

    Aulas 14/09 Orientacao a objetos, 02

--- Orientacao_Obj/src/Main.java
+++ Orientacao_Obj/src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello and welcome!");
+        Filme filmeStarWarsTerceiro = new Filme();
+
+        filmeStarWarsTerceiro.nome = "Star Wars III: A Vingança dos Sith";
+        filmeStarWarsTerceiro.anoDeLancamento = 2010;
+        filmeStarWarsTerceiro.duracaoEmMinutos = 180;
+        filmeStarWarsTerceiro.incluidoNoPlano = true;
+
+        filmeStarWarsTerceiro.exibeFichaTecnica();
+        filmeStarWarsTerceiro.setAvaliacao(8);
+        filmeStarWarsTerceiro.setAvaliacao(10);
+        filmeStarWarsTerceiro.setAvaliacao(7);
+
+        System.out.println(filmeStarWarsTerceiro.getMediaAvaliacoes());
 
     }
 }
